80c183bf7fe08b59528b68e15bb2be5ec78b9213
Remedee <zacharydanieldowns@gmail.com>
2020-02-11 10:28:27 -0600

    Exception Formatting

--- src/main/java/com/revature/exceptions/PlacesException.java
+++ src/main/java/com/revature/exceptions/PlacesException.java
@@ -1,10 +1,10 @@
 package com.revature.exceptions;
 
-public class PlacesException extends RuntimeException{
-	
+public class PlacesException extends RuntimeException {
+	private static final long serialVersionUID = -8503826726317173608L;
+
 	public PlacesException() {
 		super();
-		
 	}
 
 	public PlacesException(final String message) {
