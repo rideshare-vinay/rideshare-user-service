0e5e04806fdd7dc68704ff82bfb0d2343e9ab0c1
Annie Vo <annievo.av@hotmail.com>
2020-02-10 20:52:59 -0600

    Car and Admin Beans tests added

--- src/main/java/com/revature/Driver.java
+++ src/main/java/com/revature/Driver.java
@@ -1,8 +1,18 @@
 package com.revature;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import com.revature.beans.Admin;
+import com.revature.beans.Batch;
+import com.revature.beans.Car;
+import com.revature.beans.User;
+import com.revature.services.AdminService;
+import com.revature.services.BatchService;
+import com.revature.services.CarService;
+import com.revature.services.UserService;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -21,7 +31,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableSwagger2
 public class Driver {
-	
+
 	/**
 	 * The main method of the Driver class.
 	 * 
@@ -32,34 +42,54 @@ public class Driver {
 	public static void main(String[] args) {
 		SpringApplication.run(Driver.class, args);
 	}
-	
+
 	/**
 	 * apiInfo consists of metadata for the swagger page.
 	 * 
-	 * @return An ApiInfoBuilder which is used to add custom metadata to a swagger page.
+	 * @return An ApiInfoBuilder which is used to add custom metadata to a swagger
+	 *         page.
 	 */
-	
-    ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-            .title("RideShare User Service")
-            .description("API Documentation for User Service")
-            .version("1.0.0")
-            .build();
-    }
-	
-    /**
-     * api is needed for swagger to know what api it will be working with.
-     * 
-     * @return A Docket which selects the api, builds it and also adds custom apiInfo.
-     */
-    
+
+	ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("RideShare User Service").description("API Documentation for User Service")
+				.version("1.0.0").build();
+	}
+
+	/**
+	 * api is needed for swagger to know what api it will be working with.
+	 * 
+	 * @return A Docket which selects the api, builds it and also adds custom
+	 *         apiInfo.
+	 */
+
 	@Bean
 	public Docket api() {
-		return new Docket (DocumentationType.SWAGGER_2)
-			.select()
-			.apis(RequestHandlerSelectors.basePackage("com.revature"))
-			.build()
-			.apiInfo(apiInfo());
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.revature")).build().apiInfo(apiInfo());
 	}
+
+//	@Bean
+//	public CommandLineRunner demoData(BatchService batchService, UserService userService, AdminService adminService, CarService carService) {
+//		return args -> {
+//			Batch b = new Batch();
+//			b.setBatchLocation("UTA - Arlington, TX");
+//			b.setBatchNumber(4);
+//			batchService.addBatch(b);
+//			
+//			User u = new User();
+//			u.setUserName("admin");
+//			u.setFirstName("annie");
+//			u.setLastName("vo");
+//			u.setPhoneNumber("123456789");
+//			u.setActive(true);
+//			u.setBatch(b);
+//			userService.addUser(u);
+//			
+//			Admin a = new Admin();
+//			a.setAdminId(1);
+//			a.setUserName("admin");
+//			adminService.createAdmin(a);
+//		};
+//	}
 
 }
